c7ab069b38870d8dfba98335a54b5d0b4303b5d8
JMcB <joel.mcbride1@live.com>
2021-11-01 12:37:37 +0000

    Write solution to q1

---
+++ question_1.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+def create_triangle(n: int) -> Optional[str]:
+    """Return a string representing a triangle n levels high when printed."""
+    fill = 'x'
+    negative = '-'
+
+    if n < 0:
+        return
+
+    format_specifier = f'{{:{negative}<{n}}}'
+    lines = [format_specifier.format(fill*i) for i in range(1, n+1)]
+    return '\n'.join(lines)
+
+
+if __name__ == '__main__':
+    print(create_triangle(3))
