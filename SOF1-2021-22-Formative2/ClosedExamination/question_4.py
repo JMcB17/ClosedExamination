35fecf39b4f2647a32e4ed6a30b768afe8cc0304
JMcB <joel.mcbride1@live.com>
2021-12-04 03:35:51 +0000

    Question 4 start bugfixing

--- SOF1-2021-22-Formative2/ClosedExamination/question_4.py
+++ SOF1-2021-22-Formative2/ClosedExamination/question_4.py
@@ -4,10 +4,15 @@ from __future__ import annotations
 # camel case... AND recursion? 😳😩
 # memoization? no
 def rodCutting(prices: dict[int, int], length: int) -> list[int]:
+    for value in [*prices.keys(), *prices.values(), length]:
+        if not 0 <= value:
+            raise ValueError('All prices and length must be positive.')
+
     if not length:
-        return 0
+        return []
     
     q = float('-inf')
     for i in range(1, length + 1):
-        q = max(q, prices[i] + rodCutting(prices, length - i))
+        if i in prices:
+            q = max(q, prices[i] + rodCutting(prices, length - i))
     return q
