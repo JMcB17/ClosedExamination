82de467696fc8b0ff266bfb4615e5cb22a485c73
JMcB <joel.mcbride1@live.com>
2021-12-04 02:54:49 +0000

    Question 3 bugfix

--- SOF1-2021-22-Formative2/ClosedExamination/question_3.py
+++ SOF1-2021-22-Formative2/ClosedExamination/question_3.py
@@ -1,25 +1,30 @@
 from __future__ import annotations
 from pathlib import Path
 # support unions!
-from typing import Union
+from typing import Union, Optional
 
 
 def str_find_all(text: str, sub: str) -> list[int]:
+    len_sub = len(sub)
     location = 0
     hits = []
     while True:
-        index = text.find(sub, start=location)
+        index = text.find(sub, location)
         if index == -1:
             return hits
         else:
-            location = index
+            location = index + len_sub
             hits.append(index)
 
 
 # fuck
-def getWordsIndices(filename: Union[str, Path]) -> dict[str, list[int, int]]:
-    with open(filename, encoding='utf-8') as file:
-        text = file.read()
+def getWordsIndices(filename: Union[str, Path]) -> Optional[dict[str, list[int, int]]]:
+    try:
+        with open(filename, encoding='utf-8') as file:
+            text = file.read()
+    except FileNotFoundError:
+        # why not specified in assignment pdf only in tests?
+        return
 
     text_no_case = text.casefold()
     words_list = text_no_case.split()
