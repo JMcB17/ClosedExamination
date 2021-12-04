60c7c17242f6046d91af66254d53c2f6db8b23c6
JMcB <joel.mcbride1@live.com>
2021-12-04 01:59:32 +0000

    Question 3

---
+++ SOF1-2021-22-Formative2/ClosedExamination/question_3.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import Union
+
+def str_find_all(text: str, sub: str) -> list[int]:
+    location = 0
+    hits = []
+    while True:
+        index = text.find(sub, start=location)
+        if index == -1:
+            return hits
+        else:
+            location = index
+            hits.append(index)
+
+# fuck
+def getWordsIndices(filename: Union[str, Path]) -> dict[str, list[int, int]]:
+    with open(filename, encoding='utf-8') as file:
+        text = file.read()
+
+    text_no_case = text.casefold()
+    words_list = text_no_case.split()
+    words_set = set(words_list)
+
+    indices = {word: str_find_all(text_no_case, word) for word in words_set}
+    return indices
