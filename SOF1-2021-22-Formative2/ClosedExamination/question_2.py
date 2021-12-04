438c98b26e0e9d5f626e9423ce3b0c416ddc87bc
JMcB <joel.mcbride1@live.com>
2021-12-04 01:47:26 +0000

    Question 2

---
+++ SOF1-2021-22-Formative2/ClosedExamination/question_2.py
@@ -0,0 +1,11 @@
+# camelcase ew ew ewwww ewwwwwww
+def hammmingDistance(word1: str, word2: str):
+    if len(word1) != len(word2):
+        raise ValueError('word1 and word2 must be the same length.')
+
+    distance = 0
+    for char1, char2 in zip(word1, word2):
+        if char1 != char2:
+            distance += 1
+
+    return distance
