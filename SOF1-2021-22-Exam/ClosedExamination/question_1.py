5a483ac9e8b6becb8eefd1dd2db1c3f4e86cd757
jmcb <joelsgp@protonmail.com>
2022-01-12 11:33:12 +0000

    Docstrings for question 1

--- SOF1-2021-22-Exam/ClosedExamination/question_1.py
+++ SOF1-2021-22-Exam/ClosedExamination/question_1.py
@@ -7,6 +7,19 @@ MASS_CONSTANT = Fraction(4, 3)
 
 
 def total_mass(sphere: tuple[float]) -> float:
+    """Calculate the mass of a sphere from its radius and unit mass.
+    
+    Args:
+        sphere: A tuple (x, y, z, radius, unit_mass).
+            x, y, and z are not used by this function and can be dummy values.
+            radius and unit_mass should be floats.
+
+    Returns:
+        The mass of the sphere as a float
+
+    Raises:
+        ValueError: Radius or unit mass cannnot be less than or equal to zero.
+    """
     radius = sphere[3]
     unit_mass = sphere[4]
     if radius <= 0 or unit_mass <= 0:
@@ -16,6 +29,18 @@ def total_mass(sphere: tuple[float]) -> float:
 
 
 def centre_of_mass(spheres: set[tuple[float]]) -> tuple[float]:
+    """Calculate the overall centre of mass of a set of spheres.
+
+    Args:
+        spheres: a set of tuples like (x, y, z, radius, unit_mass), where x, y, and z are coords
+            and all the values in the tuple are floats.
+
+    Returns:
+        The centre of mass as a tuple like (x, y, z).
+
+    Raises:
+        ValueError: The set of spheres cannot be empty!
+    """
     if not spheres:
         raise ValueError('The set of spheres cannot be empty!')
 
