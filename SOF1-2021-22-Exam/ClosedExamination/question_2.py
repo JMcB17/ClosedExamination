3c81007b070ed508ac392396db2790ecb9932894
jmcb <joelsgp@protonmail.com>
2022-01-12 12:41:59 +0000

    Fix q2 stub

--- SOF1-2021-22-Exam/ClosedExamination/question_2.py
+++ SOF1-2021-22-Exam/ClosedExamination/question_2.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 
+Sphere = tuple[float]
+SphereObject = set[Sphere]
+
+
 # saved from camel case by one word function name
 # nvm the args are camel case ooooof
 # could just name them whatever since the tests only seem to use positionals but whatever
-def collide(objectA: tuple[float], objectB: tuple[float]) -> bool:
+def collide(objectA: SphereObject, objectB: SphereObject) -> bool:
     """Return whether or not the two objects are colliding.
     
     Args:
-        objectA: a tuple of float co-ordinates (x, y, z).
+        objectA: a set of tuples of float co-ordinates (x, y, z).
         objectB: same type as objectA
 
     Returns:
@@ -18,9 +22,9 @@ def collide(objectA: tuple[float], objectB: tuple[float]) -> bool:
         ValueError: Length of object tuple must be exactly four.
         ValueError: Radius cannnot be less than or equal to zero.
     """
-    if any([len(ob) != 4 for ob in (objectA, objectB)]):
-        raise ValueError('Length of object tuple must be exactly four.')
-    if any([ob[3] <= 0 for ob in (objectA, objectB)]):
+    if any([len(s) != 4 for s in objectA + objectB]):
+        raise ValueError('Length of sphere tuple must be exactly four.')
+    if any([s[3] <= 0 for s in objectA + objectB]):
         raise ValueError('Radius cannnot be less than or equal to zero.')
 
     # todo: implement
