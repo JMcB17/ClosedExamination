b825ecaac403582a60428d597800fdb2571f6415
jmcb <joelsgp@protonmail.com>
2022-05-17 17:40:25 +0100

    Implement getClosestResource

--- SOF2-2021-22-Exam/ClosedExamination/src/GameMap.java
+++ SOF2-2021-22-Exam/ClosedExamination/src/GameMap.java
@@ -1,13 +1,10 @@
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
-
 public class GameMap {
     // Public constant used to simplify the understanding of the code.
     public static final int EMPTY = 0;
     public static final int OBSTACLE = 1;
     public static final int RESOURCE = 2;
+
+    private static final int INF = Integer.MAX_VALUE;
 
     /**
      * The board representation as a 2D array of int. A value of 0 means an
@@ -47,6 +44,93 @@ public class GameMap {
         }
     }
 
+    private Position getClosestUnvisited(int[][] boardDistance) {
+        Position closest = new Position(0, 0, INF);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int currentDistance = boardDistance[x][y];
+                if (currentDistance < closest.distance) {
+                    closest.setX(x);
+                    closest.setY(y);
+                    closest.setDistance(currentDistance);
+                }
+            }
+        }
+        return closest;
+    }
+
+    // weh
+    private Position getClosestResourceFromDistances(int[][] boardDistance) {
+        Position closest = new Position(0, 0, INF);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int currentDistance = boardDistance[x][y];
+                if (board[x][y] == RESOURCE && currentDistance < closest.distance) {
+                    closest.setX(x);
+                    closest.setY(y);
+                    closest.setDistance(currentDistance);
+                }
+            }
+        }
+        return closest;
+    }
+
+    private boolean allVisited(boolean[][] boardVisited) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (!boardVisited[x][y]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private Position dijkstra(int x, int y) {
+        // Dijkstra's algorithm I guess
+        // initialise distance and visited
+        int[][] boardDistance = new int[width][height];
+        boolean[][] boardVisited = new boolean[width][height];
+        for (int y2 = 0; y2 < height; y2++) {
+            for (int x2 = 0; x2 < width; x2++) {
+                boardVisited[x2][y2] = false;
+                boardDistance[x2][y2] = INF;
+            }
+        }
+        boardDistance[x][y] = 0;
+        int currentDistance = 1;
+        
+        // loop
+        while (true) {
+            // process neighbours
+            for (int xOffset = -1; xOffset <= 1; xOffset++){
+                for (int yOffset = -1; yOffset <= 1; yOffset++) {
+                    int x2 = x + xOffset;
+                    int y2 = y = yOffset;
+                    if (!boardVisited[x2][y2] && board[x2][y2] != OBSTACLE) {
+                        boardDistance[x2][y2] = currentDistance;
+                    }
+                }
+            }
+
+
+            Position closestUnvisited = getClosestUnvisited(boardDistance);
+            x = closestUnvisited.getX();
+            y = closestUnvisited.getY();
+            currentDistance = closestUnvisited.getDistance() + 1;
+            if (closestUnvisited.distance == INF || allVisited(boardVisited)) {
+                break;
+            }
+        }
+
+        // return closest
+        Position closestResource = getClosestResourceFromDistances(boardDistance);
+        if (closestResource.getDistance() == INF) {
+            return null;
+        }
+        return closestResource;
+    }
+
     /////////////// ADD YOUR CODE BELOW ///////////////
     public Position getClosestResource(int x, int y) {
         Position closest;
@@ -57,8 +141,7 @@ public class GameMap {
         } else if (startTile == RESOURCE) {
             closest = new Position(x, y, 0);
         } else {
-            // TODO
-            closest = null;
+            closest = dijkstra(x, y);
         }
 
         return closest;
